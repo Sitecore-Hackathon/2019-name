1ca1b0f5dee0485f47a8d05a9ec5781ccb2d7950
sasha.kachanov <sasha.kachanov@americaneagle.com>
2019-03-02 01:07:00 -0600

    First stable version for droplink and droptree

---
+++ src/Foundation/GoToItem/code/Fields/GoToItemCommand.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using System;
+using Sitecore;
+
+namespace DollarName.Foundation.GoToItem.Fields
+{
+    [Serializable]
+    public class GoToItemCommand : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            ClientPipelineArgs args = new ClientPipelineArgs();
+            string fieldId = context.Parameters["id"];
+            args.Parameters["id"] = Sitecore.Context.ClientPage.ClientRequest.Form[fieldId];
+            Context.ClientPage.Start(this, "Run", args);
+        }
+
+        protected void Run(ClientPipelineArgs args)
+        {
+            if (!args.IsPostBack)
+            {
+                string id = args.Parameters["id"];
+
+                //string controlUrl = Sitecore.UIUtil.GetUri("contentlink:followlinkcommand");
+                //UrlString urlStr = new UrlString(controlUrl);
+                //urlStr.Append("id", id);
+
+                Context.ClientPage.SendMessage(this, string.Format("item:load(id={0})", id));
+
+            }
+
+        }
+    }
+}
